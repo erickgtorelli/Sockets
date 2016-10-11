357395f0454fb080d4932869234e8bb4483b56d8
Danilo Acosta <djav95@hotmail.com>
2016-10-10 20:16:21 -0600

    UI and debug mode of client and server added

--- Server.java
+++ Server.java
@@ -8,6 +8,12 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.*;
 import java.io.*;
+import javax.swing.Box;
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
  
 public class Server
 {
@@ -22,12 +28,17 @@ public class Server
     }
     public static void main(String[] args)
     {
+        guiArguments();
+    }
+    
+    
+    public static void startServer(int port2, int windowSize, boolean mode) 
+    {
         Server server = new Server();
         Util tools = new Util();
         try
         {
- 
-            int port = 25001;
+            int port = port2;
             ServerSocket serverSocket = new ServerSocket(port);
             System.out.println("Server Started and listening to the port " + port);
             socket = serverSocket.accept();
@@ -45,8 +56,10 @@ public class Server
                 //file don't compleated, continue
                 else{
                     //transfer the received content to the ArrayList
-                    System.out.println(received.getPackageContent());
-                    System.out.println(received.getPackageSec());
+                    if(mode){
+                        System.out.print("Contenido del paquete recibido: "+received.getPackageContent());
+                        System.out.print("Enviando ACK del segmento: "+received.getPackageSec());
+                    }
                     server.receivedMessage.add(received.getPackageSec(),received.getPackageContent());
                     //Returning Message                
                     //Sending the response back to the client.
@@ -83,6 +96,46 @@ public class Server
         try(  PrintWriter out = new PrintWriter( "filename.txt" )  ){
             out.println( builder.toString()); 
         }
+    }
+    
+    /*
+    * Ask for the initial values of intermediary
+    * If everthing is ok run the StartIntermediary
+    */
+    private static void guiArguments(){
+       //arguments
+        int portClient, windowSize;
+        //enter fields
+        JTextField portClientTextField = new JTextField(5);
+        JTextField windowSizeTextField = new JTextField(5);
+        //panel initialization 
+        JPanel myPanel = new JPanel();
+        myPanel.add(new JLabel("Port:"));
+        myPanel.add(portClientTextField);
+        myPanel.add(Box.createHorizontalStrut(15)); // a spacer
+        myPanel.add(new JLabel("WindowSize:"));
+        myPanel.add(windowSizeTextField);
+        JCheckBox mode = new JCheckBox("Debug mode");
+        myPanel.add(mode);
+  
+
+        //value Checking 
+        int result = JOptionPane.showConfirmDialog(null, myPanel,
+                "Please enter the initial values", JOptionPane.OK_CANCEL_OPTION);
+        if (result == JOptionPane.OK_OPTION) {
+            try {
+                portClient = Integer.parseInt(portClientTextField.getText());
+                windowSize = Integer.parseInt(windowSizeTextField.getText());
+                //check for positive values
+                if (portClient > 0 && windowSize >= 0) { 
+                    startServer(portClient,windowSize,mode.isSelected());
+                } else {
+                    JOptionPane.showMessageDialog(null, "Invalid initial values", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+            } catch (java.lang.NumberFormatException e) {
+                    JOptionPane.showMessageDialog(null, "Invalid initial values", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        } 
     }
 }
 
